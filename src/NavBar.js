78039a497d402919fa89d866cc8922e8e3460172
dennis <dennis.mwangi@student.moringaschool.com>
2023-08-01 20:58:48 +0300

    Changed login in Details

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,5 +1,6 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import Route from "Route"
+import { BrowserRouter, Route, Switch, Link } from "Route-router-dom"
+import { useState } from "react"
 
 function NavBar () {
     return (
@@ -17,7 +18,7 @@ function NavBar () {
 }
 
 function Home () {
-    return <h1>Welcom To The Best News Website</h1>
+    return <h1>Welcome To The Best News Website</h1>
 }
 
 function About () {
@@ -25,15 +26,63 @@ function About () {
 }
 
 function Login () {
+    const [firstname, setFirstname] = useState("")
+    const [lastname, setLastname] =useState("")
+    const [submittedData, setSubmittedData] = useState([])
+
+    function handleFirstName (event) {
+        setFirstname(event.target.value)
+    } 
+
+    function handleLastname (event) {
+        setLastname(event.target.value)
+    }
+
+function handleSubmit (event) {
+    event.preventDefault()
+    const formData = {
+        firstname: firstname,
+        lastname: lastname,
+    }
+    const dataArray = [...submittedData, formData]
+    setSubmittedData(dataArray)
+}
+
     return (
         <div>
-            <form>
-                <input type="text" name="FirstName" placeholder="FirstName"/>
-                <input type="text" name="SurName" placeholder="Surname"/>
+            <form onSubmit={handleSubmit}>
+                <h1>Login</h1>
+                <div>
+                <input type="text" name="firstname" placeholder="Firstname" onChange={handleFirstName}/>
+                </div>
+                <div>
+                <input type="text" name="lastname" placeholder="Lastname" onCanPlay={handleLastname}/>
+                </div>
+                <div>
                 <input type="password" name="password" placeholder="password"/>
+                </div>
+                <div>
+                <input type="submit" value="Submit" />
+                </div>
             </form>
         </div>
     )
 }
 
-export default NavBar 
+RouteDOM.render (
+    <BrowserRouter>
+    <NavBar />
+    <Switch>
+    <Route exact path="./">
+        <Home />
+    </Route>
+    <Route path="/about">
+        <About />
+    </Route>
+    <Route path="./login">
+        <Login />
+    </Route>
+    </Switch>
+    </BrowserRouter>
+)
+
