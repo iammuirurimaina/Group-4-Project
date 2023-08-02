06e862b6d75c5ec5fc939c447672c795216709f7
dennis <dennis.mwangi@student.moringaschool.com>
2023-08-02 11:00:18 +0300

    Updated NavBar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar"
+import Login from './components/Login';
+import About from './components/About';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>NewsFeed</h1>
-      {/*<NavBar />*/}
-      {/*<SearchBar />*/}
-      {/*Bookmarks />*/}
-      {/*<Categories />*/}
-      {/*<RecentSearches />*/}
-      {/*<SearchBar />*/}
-      {/*<NewsFeed />*/}
+    <NavBar />
+    <Routes>
+      <Route  path="/" element={<Home />} />
+        
+      <Route path="./about" element={<About />} />
 
-      
+      <Route path="./login" element={<Login />} />
+
+    </Routes>
 
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,29 +1,5 @@
-import Route from "Route"
-import { BrowserRouter, Route, Switch, Link } from "Route-router-dom"
+import React from 'react'
 import { useState } from "react"
-
-function NavBar () {
-    return (
-    <div>
-        <Link to="/">
-        Home
-        </Link>
-        <Link to="/about">
-        About
-        </Link>
-        <Link to="/Login">
-            Login
-        </Link>
-    </div>)
-}
-
-function Home () {
-    return <h1>Welcome To The Best News Website</h1>
-}
-
-function About () {
-    return <p>We got the best trending news around the world</p>
-}
 
 function Login () {
     const [firstname, setFirstname] = useState("")
@@ -69,20 +45,5 @@ function handleSubmit (event) {
     )
 }
 
-RouteDOM.render (
-    <BrowserRouter>
-    <NavBar />
-    <Switch>
-    <Route exact path="./">
-        <Home />
-    </Route>
-    <Route path="/about">
-        <About />
-    </Route>
-    <Route path="./login">
-        <Login />
-    </Route>
-    </Switch>
-    </BrowserRouter>
-)
 
+export default Login
